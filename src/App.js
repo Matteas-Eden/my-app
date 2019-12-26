1ffb9340c0cc4b4c3bd97e771532895bfca87b16
Matteas-Eden <mrmatteden@gmail.com>
2019-12-27 10:53:41 +1300

    Change display message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Good things come to those who wait.
         </p>
         <a
           className="App-link"
