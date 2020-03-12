f917e8656a6d595b4f8e12ed6c628b3c1e831abe
Matt Eden <mrmatteden@gmail.com>
2020-03-12 13:19:21 +1300

    Add cards

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles'
+import { Grid, Typography, Paper, useRadioGroup } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+	  flexGrow: 1,
+	},
+	paper: {
+	  height: 500,
+	  width: 300,
+	},
+  }));
+  
 
 const MainPage = () => {
+
+	const classes = useStyles();
+
 	return (
-	<div>
-		<h1>React routed here, hopefully!</h1>
-		<small>This is small text</small>
+		<Grid container direction='column' spacing={3}>
+			<Grid item xs={12}>
+				<Grid container justify="center" spacing={3}>
+				{[0, 1, 2].map(value => (
+					<Grid key={value} item>
+					<Paper className={classes.paper}>
+						<Typography>This is a card</Typography>		
+					</Paper>
+					</Grid>
+				))}
+				</Grid>
+			</Grid>
+			<Grid item xs={12}>
+				<h1>React routed here, hopefully!</h1>
+				<small>This is small text</small>
 		
-		<Link to='/my-app/users'>Show Users page</Link>
-	</div>
+				<Link to='/my-app/users'>Show Users page</Link>
+			</Grid>
+		</Grid>
 	);
 };
 
