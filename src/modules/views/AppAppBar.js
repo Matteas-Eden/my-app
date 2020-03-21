af4b15f119974cf5b1e763e5e8a7028bc54d522b
Matt Eden <mrmatteden@gmail.com>
2020-03-22 12:22:19 +1300

    Change title

--- src/modules/views/AppAppBar.js
+++ src/modules/views/AppAppBar.js
@@ -50,7 +50,7 @@ function AppAppBar(props) {
             className={classes.title}
             href="/premium-themes/onepirate/"
           >
-            {'onepirate'}
+            {'my app'}
           </Link>
           <div className={classes.right}>
             <Link
