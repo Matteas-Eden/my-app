5d2a540f137130ae3a093fefc220ac9868f59af8
Matt Eden <mrmatteden@gmail.com>
2020-03-26 19:42:35 +1300

    Remove unused star icon

--- src/modules/views/ProductHeroLayout.js
+++ src/modules/views/ProductHeroLayout.js
@@ -4,7 +4,6 @@ import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import StarIcon from '@material-ui/icons/Star';
 import { SignatureBasic as SignatureIcon } from '../components/SignatureBasic';
 
 const styles = theme => ({
