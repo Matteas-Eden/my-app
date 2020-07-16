3503edd149db9327d34d0c23e13abcce2cf2896f
Matt Eden <mrmatteden@gmail.com>
2020-07-16 20:18:08 +1200

    Cleanup unused files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,9 @@ import withRoot from '../modules/withRoot';
 // --- Post bootstrap -----
 import React from 'react';
 import ProductCategories from '../modules/views/ProductCategories';
-import ProductSmokingHero from '../modules/views/ProductSmokingHero';
 import AppFooter from '../modules/views/AppFooter';
 import ProductHero from '../modules/views/ProductHero';
 import ProductValues from '../modules/views/ProductValues';
-import ProductHowItWorks from '../modules/views/ProductHowItWorks';
-import ProductCTA from '../modules/views/ProductCTA';
 import AppAppBar from '../modules/views/AppAppBar';
 
 import { Element } from 'react-scroll';
@@ -22,9 +19,6 @@ function Index() {
       <ProductValues />
       <Element id='projects' name='projects' />
       <ProductCategories />
-      {/* <ProductHowItWorks /> */}
-      {/* <ProductCTA /> */}
-      {/* <ProductSmokingHero /> */}
       <AppFooter />
     </React.Fragment>
   );
